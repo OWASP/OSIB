06f37dfa236603f2f2dfcd0a5688cadb9f1aa9f8
sslHello <torsten.gigler@owasp.org>
2024-09-03 15:52:22 +0200

    [v0.9.1] setup.py: Updated git repository URL, used path for README.md

--- mkdocs_macro_osib_package/setup.py
+++ mkdocs_macro_osib_package/setup.py
@@ -1,16 +1,16 @@
 import setuptools
 
-with open("../README.md", "r") as fh:
+with open("README.md", "r") as fh:
     long_description    = fh.read()
 
 setuptools.setup(
     name                ="mkdocs_macro_osib",
-    version             ="0.9.0",
+    version             ="0.9.1",
     author              ="Torsten Gigler as member of the OWASP Top 10 Team 2021",
     description         ="MkDocs-Macro to use and extend the Open Security Information Base (OSIB) to provide, manage and use links between it-security documents, e.g. standards, best practices e.g. projects",
     long_description    =long_description,
     long_description_content_type="text/markdown",
-    url                 ="https://github.com/sslHello/OSIB-Test",
+    url                 ="https://github.com/OWASP/OSIB.git",
     packages            =["mkdocs_macro_osib"],
     licence             ="GPLv2: GPL - The GNU General Public License, version 2 as specified in: http://www.gnu.org/licenses/gpl-2.0"
 )
